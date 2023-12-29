ca8b1487b91ae9e66bf376c03e1db3ba565044d8
zzansol <chsk9912@gmail.com>
2023-12-29 15:11:53 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/umcmatchingcenter/UmcmatchingcenterApplication.java
@@ -0,0 +1,13 @@
+package com.example.umcmatchingcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmcmatchingcenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmcmatchingcenterApplication.class, args);
+	}
+
+}
