6048aaf19e7bfb54c5425fc223ad08e82cbd803d
cckim <r2222911r@ajou.ac.kr>
2024-01-04 18:14:46 +0900

    :sparkles: feat: "학교 엔티티 구현"
    각 필드, 연관관계, 연관관계 주인 명시(Branch.class)
    
    Related to: #5

---
+++ src/main/java/com/example/umcmatchingcenter/domain/University.java
@@ -0,0 +1,31 @@
+package com.example.umcmatchingcenter.domain;
+
+import com.example.umcmatchingcenter.domain.common.BaseEntity;
+import lombok.*;
+import org.hibernate.annotations.Cascade;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Builder
+public class University extends BaseEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToMany(mappedBy = "university", cascade = CascadeType.ALL)
+    private List<Member> members = new ArrayList<>();
+
+    @Column(nullable = false)
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "branch_id")
+    private Branch branch;
+
+}
