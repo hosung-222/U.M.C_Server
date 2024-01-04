2e486cf3970eabdec2d448244a1280b30570eddc
cckim <r2222911r@ajou.ac.kr>
2024-01-04 17:36:14 +0900

    :sparkles: feat: "멤버 엔티티 구현"
    각 필드, 연관관계 주인 명시
    
    Related to: #5

--- src/main/java/com/example/umcmatchingcenter/domain/Member.java
+++ src/main/java/com/example/umcmatchingcenter/domain/Member.java
@@ -1,18 +1,20 @@
 package com.example.umcmatchingcenter.domain;
 
 import com.example.umcmatchingcenter.domain.common.BaseEntity;
+import com.example.umcmatchingcenter.domain.enums.MemberPart;
+import com.example.umcmatchingcenter.domain.enums.MemberRole;
+import com.example.umcmatchingcenter.domain.enums.MemberMatchingStatus;
+
+import javax.persistence.*;
+
 import com.example.umcmatchingcenter.domain.enums.MemberStatus;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -21,11 +23,60 @@ import lombok.NoArgsConstructor;
 @Builder
 public class Member extends BaseEntity {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String name_nickname;
+
+    @ManyToOne
+    @JoinColumn(name = "university_id")
+    private University university;
+
+    @Column(nullable = false)
+    private String phoneNumber;
+
+    private String portfolio;
+
+    private String profileImage;
+
+    @Column(nullable = false)
+    private int generation;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Enumerated(EnumType.STRING)
+    private MemberStatus memberStatus;
+
+    @Enumerated(EnumType.STRING)
+    private MemberRole role;
+
+    @Enumerated(EnumType.STRING)
+    private MemberPart part;
 
     @Enumerated(EnumType.STRING)
     @Column(columnDefinition = "VARCHAR(15) DEFAULT 'ACTIVE'")
-    private MemberStatus status;
+    private MemberMatchingStatus matchingStatus;
+
+    @OneToMany(mappedBy = "member_id", cascade = CascadeType.ALL)
+    private List<Alarm> alarms = new ArrayList<>();
+
+    @OneToMany(mappedBy = "pm", cascade = CascadeType.ALL)
+    private List<Chat> pmChats = new ArrayList<>();
+
+    @OneToMany(mappedBy = "inquirer", cascade = CascadeType.ALL)
+    private List<Chat> inquirerChats = new ArrayList<>();
+
+    @ManyToOne
+    @JoinColumn(name = "project_id")
+    private Project project;
 
 }
