138215598fe031edbdef0e3ad4fc1b988b6298b6
cckim <r2222911r@ajou.ac.kr>
2024-01-04 18:26:02 +0900

    :sparkles: feat: "Member 관련 Enum 타입 생성"
    멤버 매칭 상태, 맴버 파트, 멤버 권한, 멤버 상태 Enum
    
    Related to: #5

---
+++ src/main/java/com/example/umcmatchingcenter/domain/enums/MemberPart.java
@@ -0,0 +1,7 @@
+package com.example.umcmatchingcenter.domain.enums;
+
+public enum MemberPart {
+    DESIGNER, WEB, SERVER, ANDROID, PM
+}
+
+
---
+++ src/main/java/com/example/umcmatchingcenter/domain/enums/MemberStatus.java
@@ -0,0 +1,5 @@
+package com.example.umcmatchingcenter.domain.enums;
+
+public enum MemberStatus {
+    WAIT_ACCEPTANCE, CURRENT, PREVIOUS, EXIT
+}
