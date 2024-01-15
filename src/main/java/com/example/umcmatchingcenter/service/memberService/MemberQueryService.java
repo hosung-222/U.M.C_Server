acc7627f62255f68eb98ddede3a96159f6279a33
hosung-222 <gik11kr@naver.com>
2024-01-15 09:35:42 +0900

    :sparkles: feat: 챌린저 조회 서비스 구현
    관리자 챌린저 조회 페이지 API 서비스 구현
    
    Fixes: #31

--- src/main/java/com/example/umcmatchingcenter/service/memberService/MemberQueryService.java
+++ src/main/java/com/example/umcmatchingcenter/service/memberService/MemberQueryService.java
@@ -1,12 +1,19 @@
 package com.example.umcmatchingcenter.service.memberService;
 
 
-import com.example.umcmatchingcenter.apiPayload.ApiResponse;
-import com.example.umcmatchingcenter.apiPayload.code.status.ErrorStatus;
+import static com.example.umcmatchingcenter.domain.enums.MemberRole.*;
+
 import com.example.umcmatchingcenter.domain.Member;
+import com.example.umcmatchingcenter.domain.enums.MemberMatchingStatus;
+import com.example.umcmatchingcenter.domain.enums.MemberRole;
+import com.example.umcmatchingcenter.dto.MemberDTO.MemberResponseDTO.ChallengerInfoDTO;
 import com.example.umcmatchingcenter.repository.MemberRepository;
+import java.util.List;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -27,4 +34,11 @@ public class MemberQueryService {
         return target.get();
     }
 
+    public List<ChallengerInfoDTO> getChallengerList(MemberMatchingStatus memberMatchingStatus, int page) {
+        Page<Member> members = memberRepository.findByGenerationAndRoleAndMatchingStatus(5, ROLE_CHALLENGER,
+                memberMatchingStatus, PageRequest.of(page, 10));
+
+
+        return null;
+    }
 }
