32f4a365d48deb10562be43e417e01eca17c2db4
hosung-222 <gik11kr@naver.com>
2024-01-15 14:48:24 +0900

    :bug: chore: LoginResponseDto 삭제
    깃 오류로 인한 파일 삭제
    
    Fixes: #24

---
+++ src/main/java/com/example/umcmatchingcenter/dto/MemberDTO/LoginResponseDTO.java
@@ -0,0 +1,2 @@
+package com.example.umcmatchingcenter.dto.MemberDTO;public class LoginResponseDTO {
+}
---
+++ src/main/java/com/example/umcmatchingcenter/dto/MemberDto/LoginResponseDto.java
@@ -0,0 +1,18 @@
+package com.example.umcmatchingcenter.dto.MemberDTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginResponseDTO {
+
+    private String email;
+    private String accessToken;
+    private String refreshToken;
+
+}
