4481d21d8d7501c9372908402fc0807c564be7c8
cckim <r2222911r@ajou.ac.kr>
2024-01-15 12:10:48 +0900

    :zap: chore: 생성자 어노테이션 추가
    생성자 어노테이션 추가
    
    Related to: #23

--- src/main/java/com/example/umcmatchingcenter/dto/projectDto/TotalMatchingResponseDto.java
+++ src/main/java/com/example/umcmatchingcenter/dto/projectDto/TotalMatchingResponseDto.java
@@ -1,10 +1,14 @@
 package com.example.umcmatchingcenter.dto.projectDto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class TotalMatchingResponseDto {
 
     private int nowMatchingNum;
