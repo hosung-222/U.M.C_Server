1544c8c960fc7059c6c1f4b65d3c63472f7a8f86
cckim <r2222911r@ajou.ac.kr>
2024-01-16 15:11:52 +0900

    :zap: chore: 생성자 어노테이션 추가
    생성자 어노테이션 추가
    
    Related to: #23

--- src/main/java/com/example/umcmatchingcenter/dto/projectDto/ApplicantInfoResponseDto.java
+++ src/main/java/com/example/umcmatchingcenter/dto/projectDto/ApplicantInfoResponseDto.java
@@ -3,17 +3,21 @@ package com.example.umcmatchingcenter.dto.projectDto;
 import com.example.umcmatchingcenter.domain.University;
 import com.example.umcmatchingcenter.domain.enums.MemberMatchingStatus;
 import com.example.umcmatchingcenter.domain.enums.MemberPart;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ApplicantInfoResponseDto {
 
-    String nameNickname;
-    String university;
-    MemberPart memberPart;
-    String profileImage;
-    MemberMatchingStatus memberMatchingStatus;
+    private String nameNickname;
+    private String university;
+    private MemberPart memberPart;
+    private String profileImage;
+    private MemberMatchingStatus memberMatchingStatus;
 
 }
