ab8804ca2f29b5f4dab8759c40a719519004e880
Kimdongjun <r2222911r@ajou.ac.kr>
2024-01-19 00:45:03 +0900

    :recycle: refactor: 각종 쿼리 서비스 메소드 분리에 따른 생성자 주입 및 각종 코드 리팩토링
    각종 쿼리 서비스 메소드 분리에 따른 생성자 주입 및 각종 코드 리팩토링
    
    Related to: #23

--- src/main/java/com/example/umcmatchingcenter/service/myProjectService/ProjectService.java
+++ src/main/java/com/example/umcmatchingcenter/service/myProjectService/ProjectService.java
@@ -8,7 +8,6 @@ import com.example.umcmatchingcenter.converter.myProject.ProjectConverter;
 import com.example.umcmatchingcenter.converter.myProject.TotalMatchingConverter;
 import com.example.umcmatchingcenter.domain.Project;
 import com.example.umcmatchingcenter.domain.enums.MemberMatchingStatus;
-import com.example.umcmatchingcenter.domain.enums.MemberPart;
 import com.example.umcmatchingcenter.domain.enums.RecruitmentStatus;
 import com.example.umcmatchingcenter.domain.mapping.ProjectVolunteer;
 import com.example.umcmatchingcenter.domain.mapping.Recruitment;
@@ -16,12 +15,15 @@ import com.example.umcmatchingcenter.dto.projectDTO.ApplicantInfoResponseDTO;
 import com.example.umcmatchingcenter.dto.projectDTO.MyProjectResponseDTO;
 import com.example.umcmatchingcenter.dto.projectDTO.PartMatchingResponseDTO;
 import com.example.umcmatchingcenter.dto.projectDTO.TotalMatchingResponseDTO;
+import com.example.umcmatchingcenter.service.queryService.MemberQueryService;
+import com.example.umcmatchingcenter.service.queryService.ProjectQueryService;
+import com.example.umcmatchingcenter.service.queryService.ProjectVolunteerQueryService;
+import com.example.umcmatchingcenter.service.queryService.RecruitmentQueryService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
@@ -30,9 +32,13 @@ import java.util.stream.Collectors;
 @Service
 @RequiredArgsConstructor
 @Slf4j
+@Transactional
 public class ProjectService {
 
+    private final MemberQueryService memberQueryService;
     private final ProjectQueryService projectQueryService;
+    private final ProjectVolunteerQueryService projectVolunteerQueryService;
+    private final RecruitmentQueryService recruitmentQueryService;
 
     public MyProjectResponseDTO myProject() {
         return MyProjectConverter.toMyProjectResponseDto(
@@ -111,23 +117,23 @@ public class ProjectService {
     public String pass(Long memberId) {
 
         // 인원 만석 시 예외처리
-        if (isFull(memberId)) {
+        if (projectQueryService.isFull(memberId)) {
             throw new MyProjectHandler(ErrorStatus.NO_MORE_APPLICANT);
         }
 
-        if (projectQueryService.getProjectVolunteer(memberId) != null && projectQueryService.getProjectVolunteer(memberId).isPresent()) {
+        if (projectVolunteerQueryService.getProjectVolunteer(memberId) != null && projectVolunteerQueryService.getProjectVolunteer(memberId).isPresent()) {
 
-            projectQueryService.getMember(memberId).setProject(projectQueryService.getProject());
-            projectQueryService.getMember(memberId).setMatchingStatus(MemberMatchingStatus.MATCH);
-            Recruitment recruitment = projectQueryService.getRecruitment(
-                    projectQueryService.getMember(memberId).getPart(),
+            memberQueryService.getMember(memberId).setProject(projectQueryService.getProject());
+            memberQueryService.getMember(memberId).setMatchingStatus(MemberMatchingStatus.MATCH);
+            Recruitment recruitment = recruitmentQueryService.getRecruitment(
+                    memberQueryService.getMember(memberId).getPart(),
                     projectQueryService.getProject());
             int nowRecruitment = recruitment.getNowRecruitment();
             recruitment.setNowRecruitment(++nowRecruitment);
-            if(isFull(memberId)){
+            if(projectQueryService.isFull(memberId)){
                 recruitment.setRecruitmentStatus(RecruitmentStatus.FULL);
             }
-            return projectQueryService.getMember(memberId).getNameNickname();
+            return memberQueryService.getMember(memberId).getNameNickname();
         }
         throw new MyProjectHandler(ErrorStatus.NO_SUCH_APPLICANT);
     }
@@ -135,27 +141,13 @@ public class ProjectService {
     @Transactional
     public String fail(Long memberId) {
 
-        Optional<ProjectVolunteer> foundApplication = projectQueryService.getProjectVolunteer(memberId);
+        Optional<ProjectVolunteer> foundApplication = projectVolunteerQueryService.getProjectVolunteer(memberId);
 
         if (foundApplication != null && foundApplication.isPresent()) {
-            projectQueryService.getMember(memberId).setMatchingStatus(MemberMatchingStatus.NON);
-            return projectQueryService.getMember(memberId).getNameNickname();
+            memberQueryService.getMember(memberId).setMatchingStatus(MemberMatchingStatus.NON);
+            return memberQueryService.getMember(memberId).getNameNickname();
         }
         throw new MyProjectHandler(ErrorStatus.NO_SUCH_APPLICANT);
-    }
-
-    public Boolean isFull(Long memberId) {
-
-        if (projectQueryService.getMember(memberId) == null) {
-            throw new MyProjectHandler(ErrorStatus.NO_SUCH_APPLICANT);
-        }
-        MemberPart memberPart = projectQueryService.getMember(memberId).getPart();
-        Optional<Recruitment> recruitment = projectQueryService.getProject().getRecruitments().stream()
-                .filter(part -> part.getPart().equals(memberPart)).findAny();
-        if (recruitment.get().getNowRecruitment() == recruitment.get().getTotalRecruitment()){
-            return true;
-        }
-        return false;
     }
 
     private double calculateCompetitionRate(int totalRecruitment, int totalApplicants) {
