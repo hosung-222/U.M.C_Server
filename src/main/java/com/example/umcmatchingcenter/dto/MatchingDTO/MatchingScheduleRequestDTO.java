12f7d8774482be728e104e8a934041d5ec003a2f
Nahyun Kim <hedghog1004@naver.com>
2024-01-23 18:19:33 +0900

    :sparkles: feat: 매칭 일정 요청 스키마 추가
    Related to: #54

--- src/main/java/com/example/umcmatchingcenter/dto/MatchingDTO/MatchingScheduleRequestDTO.java
+++ src/main/java/com/example/umcmatchingcenter/dto/MatchingDTO/MatchingScheduleRequestDTO.java
@@ -2,6 +2,7 @@ package com.example.umcmatchingcenter.dto.MatchingDTO;
 
 import com.example.umcmatchingcenter.domain.enums.ScheduleColor;
 import com.sun.istack.NotNull;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 public class MatchingScheduleRequestDTO {
@@ -10,14 +11,19 @@ public class MatchingScheduleRequestDTO {
     public static class MatchingScheduleDTO {
 
         @NotNull
+        @Schema(description =  "일정 이름", example = "일정 이름")
         String title;
 
+        @Schema(description =  "일정 메모", example = "일정 메모 (선택)")
         String description;
 
+        @Schema(description = "시작일", example = "2024-01-25")
         String startDate;
 
+        @Schema(description = "종료일", example = "2024-02-01")
         String endDate;
 
+        @Schema(description = "일정 색상", example = "일정 색상 (WHITE, RED, ORANGE, ..)")
         ScheduleColor scheduleColor;
     }
 }
