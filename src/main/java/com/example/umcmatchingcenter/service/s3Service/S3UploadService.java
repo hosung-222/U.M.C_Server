6b55030ff3ec9c9a9f62dbef22834425fcbeaecf
hosung-222 <gik11kr@naver.com>
2024-01-30 16:46:08 +0900

    :sparkles: feat: UUID+원본파일 이미지 업로득 구현
    S3 업로드시 UUID + 원본 파일 이름으로 업로드 uploadFile 구현
    
    Fixes: #67

--- src/main/java/com/example/umcmatchingcenter/service/s3Service/S3UploadService.java
+++ src/main/java/com/example/umcmatchingcenter/service/s3Service/S3UploadService.java
@@ -2,7 +2,12 @@ package com.example.umcmatchingcenter.service.s3Service;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.example.umcmatchingcenter.apiPayload.code.status.ErrorStatus;
+import com.example.umcmatchingcenter.apiPayload.exception.handler.MemberHandler;
+import com.example.umcmatchingcenter.domain.Uuid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -10,6 +15,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class S3UploadService {
 
@@ -33,5 +39,19 @@ public class S3UploadService {
         amazonS3.deleteObject(bucket, path);
     }
 
+    public String uploadFile(String uuid, MultipartFile file){
+        ObjectMetadata metadata = new ObjectMetadata();
+        String saveName = uuid + file.getOriginalFilename();
+
+        metadata.setContentLength(file.getSize());
+        try {
+            amazonS3.putObject(new PutObjectRequest(bucket, saveName, file.getInputStream(), metadata));
+        }catch (IOException e){
+            log.error("error at AmazonS3Manager uploadFile : {}", (Object) e.getStackTrace());
+            throw new MemberHandler(ErrorStatus.MEMBER_PROFILE_ERROR);
+        }
+
+        return amazonS3.getUrl(bucket, saveName).toString();
+    }
 
 }
