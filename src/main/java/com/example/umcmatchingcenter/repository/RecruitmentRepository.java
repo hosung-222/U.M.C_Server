76e6152f823b0bddb7f73bae61a374f813d505e4
hosung-222 <gik11kr@naver.com>
2024-02-08 15:40:57 +0900

    :sparkles: feat: 현재 프로젝트 모집 여부 조회 쿼리
    프로젝트에 해당하는 모집상태 여부 조회 쿼리 Data JPA로 구현
    
    Fixes: #96

--- src/main/java/com/example/umcmatchingcenter/repository/RecruitmentRepository.java
+++ src/main/java/com/example/umcmatchingcenter/repository/RecruitmentRepository.java
@@ -2,7 +2,9 @@ package com.example.umcmatchingcenter.repository;
 
 import com.example.umcmatchingcenter.domain.Project;
 import com.example.umcmatchingcenter.domain.enums.MemberPart;
+import com.example.umcmatchingcenter.domain.enums.RecruitmentStatus;
 import com.example.umcmatchingcenter.domain.mapping.Recruitment;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -11,4 +13,5 @@ public interface RecruitmentRepository extends JpaRepository<Recruitment, Long> 
 
     Optional<Recruitment> findByPartAndProject(MemberPart part, Project project);
 
+    List<Recruitment> findByProjectAndRecruitmentStatus(Project project, RecruitmentStatus recruitmentStatus);
 }
