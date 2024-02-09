643c445763553e0a4fc116ff713487fe72d32288
Kimdongjun <r2222911r@ajou.ac.kr>
2024-02-09 14:22:22 +0900

    :bug: chore: 상호평가 관련 메서드 예외 추가
    상호평가 관련 메서드 예외 추가

--- src/main/java/com/example/umcmatchingcenter/service/evaluationService/EvaluationQueryService.java
+++ src/main/java/com/example/umcmatchingcenter/service/evaluationService/EvaluationQueryService.java
@@ -4,6 +4,7 @@ import com.example.umcmatchingcenter.apiPayload.code.status.ErrorStatus;
 import com.example.umcmatchingcenter.apiPayload.exception.handler.EvaluationHandler;
 import com.example.umcmatchingcenter.domain.Evaluation;
 import com.example.umcmatchingcenter.domain.Member;
+import com.example.umcmatchingcenter.domain.Project;
 import com.example.umcmatchingcenter.repository.EvaluationRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -11,6 +12,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
+
+import static com.example.umcmatchingcenter.apiPayload.code.status.ErrorStatus.*;
 
 @Transactional(readOnly = true)
 @Service
@@ -23,7 +26,6 @@ public class EvaluationQueryService {
     public Evaluation getEvaluation(Member evaluator, Member evaluatee) {
         Optional<Evaluation> evaluation =
                 evaluationRepository.findByEvaluatorAndEvaluatee(evaluator, evaluatee);
-
         if (evaluation.isPresent() && evaluation != null) {
             return evaluation.get();
         }
@@ -33,7 +35,13 @@ public class EvaluationQueryService {
     public void isExist(Member evaluator, Member evaluatee) {
         Evaluation evaluation = getEvaluation(evaluator, evaluatee);
         if (evaluation != null) {
-            throw new EvaluationHandler(ErrorStatus.ALREADY_EVALUATED_MEMBER);
+            throw new EvaluationHandler(ALREADY_EVALUATED_MEMBER);
+        }
+    }
+
+    public void contains(Project project, Member evaluatee) {
+        if (!project.getMembers().contains(evaluatee)) {
+            throw new EvaluationHandler(TEAMMATE_NOT_FOUND);
         }
     }
 
