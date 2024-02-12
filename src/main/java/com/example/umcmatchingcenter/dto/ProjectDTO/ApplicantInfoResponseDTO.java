19e208ab638511b2163f247780bf226e6c0e3727
Kimdongjun <r2222911r@ajou.ac.kr>
2024-02-12 16:37:46 +0900

    :recycle: chore: 내 프로젝트 response 필드 인자 추가
    response memberId 필드 추가

--- src/main/java/com/example/umcmatchingcenter/dto/ProjectDTO/ApplicantInfoResponseDTO.java
+++ src/main/java/com/example/umcmatchingcenter/dto/ProjectDTO/ApplicantInfoResponseDTO.java
@@ -13,6 +13,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class ApplicantInfoResponseDTO {
 
+    private Long memberId;
     private String nameNickname;
     private String university;
     private MemberPart memberPart;
