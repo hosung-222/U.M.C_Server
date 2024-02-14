0d59145b3b5c8f2e8cfd78e3352f1ac990a02a24
zzansol <chsk9912@gmail.com>
2024-02-14 16:05:06 +0900

    :sparkles: feat: "LandingPageRepository 추가"
    LandingPageRepository 추가
    
    fix: #116

---
+++ src/main/java/com/example/umcmatchingcenter/repository/LandingPageRepository.java
@@ -0,0 +1,7 @@
+package com.example.umcmatchingcenter.repository;
+
+import com.example.umcmatchingcenter.domain.LandingPage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LandingPageRepository extends JpaRepository<LandingPage, Long> {
+}
