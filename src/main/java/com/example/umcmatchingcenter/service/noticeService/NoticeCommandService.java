ce62f4787bca9dcf1cece6bdd5b14c1a1ed271db
zzansol <chsk9912@gmail.com>
2024-02-16 20:13:40 +0900

    :recycle: refactor: "공지 등록 시 이미지 저장 로직 수정"
    공지 등록 시 이미지 저장 로직 수정
    
    fix: #133

--- src/main/java/com/example/umcmatchingcenter/service/noticeService/NoticeCommandService.java
+++ src/main/java/com/example/umcmatchingcenter/service/noticeService/NoticeCommandService.java
@@ -1,11 +1,15 @@
 package com.example.umcmatchingcenter.service.noticeService;
 
+import com.example.umcmatchingcenter.converter.ImageConverter;
 import com.example.umcmatchingcenter.converter.NoticeConverter;
 import com.example.umcmatchingcenter.domain.Branch;
+import com.example.umcmatchingcenter.domain.Image;
 import com.example.umcmatchingcenter.domain.Member;
 import com.example.umcmatchingcenter.domain.Notice;
 import com.example.umcmatchingcenter.domain.enums.AlarmType;
+import com.example.umcmatchingcenter.domain.mapping.NoticeImage;
 import com.example.umcmatchingcenter.dto.noticeDTO.NoticeRequestDTO;
+import com.example.umcmatchingcenter.repository.NoticeImageRepository;
 import com.example.umcmatchingcenter.repository.NoticeRepository;
 import com.example.umcmatchingcenter.service.AlarmService.AlarmCommandService;
 import com.example.umcmatchingcenter.service.memberService.MemberQueryService;
@@ -17,6 +21,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -29,14 +34,20 @@ public class NoticeCommandService {
     private final S3UploadService s3UploadService;
     private final MemberQueryService memberQueryService;
     private final NoticeQueryService noticeQueryService;
+    private final NoticeImageRepository noticeImageRepository;
 
-    public Notice addNotice(NoticeRequestDTO.AddNoticeDTO request, List<MultipartFile> imageListString, String memberName){
+    public Notice addNotice(NoticeRequestDTO.AddNoticeDTO request, List<MultipartFile> imageList, String memberName){
 
         Notice notice = NoticeConverter.toNotice(request);
 
         Member member = memberQueryService.findMemberByName(memberName);
 
-        s3UploadService.uploadFileList(imageListString);
+        List<NoticeImage> noticeImageList = imageList.stream()
+                .map(s3UploadService::saveFile)
+                .map(image -> ImageConverter.toNoticeImage(image, notice))
+                .collect(Collectors.toList());
+
+        noticeImageRepository.saveAll(noticeImageList);
 
         Branch branch = member.getUniversity().getBranch();
         alarmCommandService.sendToBranch(branch, AlarmType.NOTICE, "새로운 공지사항이 등록되었습니다.");
