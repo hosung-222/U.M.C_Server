eedd34ca7fa4f186e852a050f5bbbea080b00ee7
zzansol <chsk9912@gmail.com>
2024-02-16 19:41:12 +0900

    :sparkles: feat: "NoticeImageRepository 추가"
    NoticeImageRepository 추가
    
    fix: #133

---
+++ src/main/java/com/example/umcmatchingcenter/repository/NoticeImageRepository.java
@@ -0,0 +1,7 @@
+package com.example.umcmatchingcenter.repository;
+
+import com.example.umcmatchingcenter.domain.mapping.NoticeImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NoticeImageRepository extends JpaRepository<NoticeImage, Long> {
+}
