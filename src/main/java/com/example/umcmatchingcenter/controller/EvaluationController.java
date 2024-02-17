489ce18c210d3477663daa4b6f4748d9e52045b4
Kimdongjun <r2222911r@ajou.ac.kr>
2024-02-17 19:42:23 +0900

    :sparkles: feat: 내 평가 API 생성
    내 평가 API 생성

--- src/main/java/com/example/umcmatchingcenter/controller/EvaluationController.java
+++ src/main/java/com/example/umcmatchingcenter/controller/EvaluationController.java
@@ -3,6 +3,7 @@ package com.example.umcmatchingcenter.controller;
 import com.example.umcmatchingcenter.apiPayload.ApiResponse;
 import com.example.umcmatchingcenter.dto.evaluationDTO.EvaluationRequestDTO;
 import com.example.umcmatchingcenter.dto.evaluationDTO.EvaluationResponseDTO;
+import com.example.umcmatchingcenter.dto.evaluationDTO.MyEvaluationResponseDTO;
 import com.example.umcmatchingcenter.service.evaluationService.EvaluationService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -52,6 +53,19 @@ public class EvaluationController {
                                               @PathVariable Long memberId) {
         evaluationService.saveEvaluation(dto, memberId);
         return ApiResponse.onSuccess("평가 저장 완료");
+    }
+
+    @GetMapping("/getMyEvaluations")
+    @Operation(summary = "내 평가 보기 api")
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "COMMON200", description = "OK, 성공"),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "EVALUATION4001", description = "평가가 존재하지 않습니다."),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "EVALUATION4002", description = "해당 프로젝트의 멤버가 아닙니다."),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "EVALUATION4003", description = "이미 평가가 완료된 멤버입니다."),
+    })
+    @PreAuthorize("isAuthenticated()")
+    public ApiResponse<List<MyEvaluationResponseDTO>> getMyEvaluation() {
+        return ApiResponse.onSuccess(evaluationService.getMyEvaluations());
     }
 
 }
