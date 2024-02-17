0f9f986fb8e0f31d616de8d3eb4190720bfeec59
Kimdongjun <r2222911r@ajou.ac.kr>
2024-02-17 19:43:25 +0900

    :sparkles: feat: 내 평가 쿼리서비스 메서드 추가
    내 평가 쿼리서비스 메서드 추가

--- src/main/java/com/example/umcmatchingcenter/service/evaluationService/EvaluationQueryService.java
+++ src/main/java/com/example/umcmatchingcenter/service/evaluationService/EvaluationQueryService.java
@@ -1,16 +1,19 @@
 package com.example.umcmatchingcenter.service.evaluationService;
 
-import com.example.umcmatchingcenter.apiPayload.code.status.ErrorStatus;
 import com.example.umcmatchingcenter.apiPayload.exception.handler.EvaluationHandler;
+import com.example.umcmatchingcenter.apiPayload.exception.handler.ProjectHandler;
 import com.example.umcmatchingcenter.domain.Evaluation;
 import com.example.umcmatchingcenter.domain.Member;
 import com.example.umcmatchingcenter.domain.Project;
 import com.example.umcmatchingcenter.repository.EvaluationRepository;
+import com.example.umcmatchingcenter.service.memberService.MemberQueryService;
+import com.example.umcmatchingcenter.service.projectService.ProjectQueryService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 import static com.example.umcmatchingcenter.apiPayload.code.status.ErrorStatus.*;
@@ -22,6 +25,8 @@ import static com.example.umcmatchingcenter.apiPayload.code.status.ErrorStatus.*
 public class EvaluationQueryService {
 
     private final EvaluationRepository evaluationRepository;
+    private final MemberQueryService memberQueryService;
+    private final ProjectQueryService projectQueryService;
 
     public Evaluation getEvaluation(Member evaluator, Member evaluatee) {
         Optional<Evaluation> evaluation =
@@ -44,6 +49,21 @@ public class EvaluationQueryService {
         if (!project.getMembers().contains(evaluatee)) {
             throw new EvaluationHandler(TEAMMATE_NOT_FOUND);
         }
+    }
+
+    public List<Evaluation> getMyEvaluation() {
+
+        Member member = memberQueryService.getCurrentLoginMember();
+        List<Evaluation> evaluations = evaluationRepository.findByEvaluatee(member);
+
+        if(member.getProject()==null){
+            throw new ProjectHandler(PROJECT_NOT_EXIST);
+        }
+        if(evaluations.isEmpty()){
+            throw new EvaluationHandler(EVALUATION_NOT_FOUND);
+        }
+
+        return evaluations;
     }
 
 
